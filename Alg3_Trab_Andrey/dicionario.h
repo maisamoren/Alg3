08b10072ba4df10046767ea3d9213ff27bb02629
Maisa <117362944+maisamoren@users.noreply.github.com>
2024-08-12 11:10:11 -0300

    Update dicionario.h

--- Alg3_Trab_Andrey/dicionario.h
+++ Alg3_Trab_Andrey/dicionario.h
@@ -13,11 +13,11 @@ char caracterAtual (nodeTrie *nodo);
 
 void inverterVetor (char vetor[]);
 
-void imprimePalavraAchada (nodeTrie *nodo, FILE *outputFile, int contagem);
+void imprimePalavraAchada (nodeTrie *nodo, FILE *outputFile, int x);
 
 int verificaIgualdadeLetra (char key1, char key2);
 
-int palavrasSemelhantes (nodeTrie *nodo, char *key, int letraAtual, int totalLetras, int errosMax, int errosAtual, FILE *outputFile, int contagem);
+int palavrasSemelhantes (nodeTrie *nodo, char *key, int letraAtual, int totalLetras, int errosMax, int errosAtual, FILE *outputFile, nodeTrie *vetor[], int *x);
 
 void loadWords (nodeTrie *root, const char *filename);
 
